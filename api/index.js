fd976eae2ff29991aaff1024a508353817fa9797
ZeroDayCops <zerodaycops@gmail.com>
2026-07-30 23:57:49 +0530

    fix(prisma): add explicit require for .prisma/client/default.js in api/index.js for Vercel bundler

--- api/index.js
+++ api/index.js
@@ -1,3 +1,11 @@
+try {
+  require('../SchedulerAgent/backend/node_modules/.prisma/client/default.js');
+} catch (e) {}
+try {
+  require('../SchedulerAgent/backend/node_modules/.prisma/client/index.js');
+} catch (e) {}
+
 const app = require('../SchedulerAgent/backend/src/index.js');
 
 module.exports = app;
+
